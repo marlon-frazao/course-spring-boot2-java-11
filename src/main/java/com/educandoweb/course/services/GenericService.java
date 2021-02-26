45f995c1f51c8d611705c86c9c8221d524dc5bd3
marlon-frazao <frazao.marlon@gmail.com>
2021-02-26 13:38:09 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/GenericService.java
+++ src/main/java/com/educandoweb/course/services/GenericService.java
@@ -4,33 +4,42 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import com.educandoweb.course.util.Convertible;
 
 public interface GenericService<T extends Convertible<DTO>, DTO, ID> {
-	
+
 	JpaRepository<T, ID> getRepository();
-	
+
 	default DTO findById(ID id) {
 		Optional<T> obj = getRepository().findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id)).convert();
 	}
-	
+
 	default List<DTO> findAll() {
 		List<T> list = getRepository().findAll();
 		return list.stream().map(x -> x.convert()).collect(Collectors.toList());
 	}
-	
+
 	default DTO insert(T obj) {
 		return getRepository().save(obj).convert();
 	}
-	
+
 	default void delete(ID id) {
-		getRepository().deleteById(id);
+		try {
+			getRepository().deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
-	
+
 	default DTO update(ID id, T obj) {
 		T entity = getRepository().getOne(id);
 		updateData(entity, obj);
