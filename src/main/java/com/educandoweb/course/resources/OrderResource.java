442254ff45afa5959aa9f35168d9e971c7681477
marlon-frazao <frazao.marlon@gmail.com>
2021-03-02 10:39:48 -0300

    Inserindo pedido

--- src/main/java/com/educandoweb/course/resources/OrderResource.java
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -1,15 +1,20 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.dto.OrderDTO;
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.services.OrderService;
 
 @RestController
@@ -27,5 +32,12 @@ public class OrderResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody Order obj) {
+		OrderDTO inserido = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(inserido.getId()).toUri();
+		return ResponseEntity.created(uri).body(inserido);
 	}
 }
--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -1,17 +1,35 @@
 package com.educandoweb.course.services;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.dto.OrderDTO;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.Payment;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.OrderItemRepository;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.ProductRepository;
+import com.educandoweb.course.repositories.UserRepository;
 
 @Service
 public class OrderService implements GenericService<Order, OrderDTO, Long>{
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
 	public JpaRepository<Order, Long> getRepository() {
@@ -22,5 +40,27 @@ public class OrderService implements GenericService<Order, OrderDTO, Long>{
 	public void updateData(Order entity, Order obj) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public OrderDTO insert(Order obj) {
+		Order order = new Order(null, obj.getMoment(), obj.getOrderStatus(), userRepository.getOne(obj.getClient().getId()));
+		order = repository.save(order);
+		
+		Set<OrderItem> items = new HashSet<>();
+		
+		for(OrderItem i : obj.getItems()) {
+			Product product = productRepository.getOne(i.getProduct().getId());
+			items.add(new OrderItem(order, product , i.getQuantity(), product.getPrice()));
+		}
+		
+		orderItemRepository.saveAll(items);
+		
+		if(obj.getPayment().getMoment() != null) {
+			order.setPayment(new Payment(null, obj.getPayment().getMoment(), order));
+			return repository.save(order).convert();
+		}
+		
+		return order.convert();
 	}
 }
